eddfaf103a6dcf8fae23e7a80b6ddf77f660734e
5redssssr <112356450+5redssssr@users.noreply.github.com>
2022-08-29 17:14:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
+global.owner = ['94785946099']
+global.premium = ['94785946099']
+global.ownernomer = '94785946099'
 global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
 global.botname = 'VIHANGA MD BOT'
 global.footer = 'VIHANGA MD'
